737a6544448b2e3c6ed7fef14b55c0f24a8f707d
Sachin <sachinkmr3112@gmail.com>
2016-09-14 13:54:36 +0530

    maven structure

---
+++ src/main/java/web/analytics/App.java
@@ -0,0 +1,13 @@
+package web.analytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
